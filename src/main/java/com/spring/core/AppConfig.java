bdad6dd5722825c3b9091888bb6c187b9babe53a
kellykang-tech <kellykang13@gmail.com>
2021-10-02 14:42:49 +0900

    순수 자바 코드만으로 DI 적용

--- src/main/java/com/spring/core/AppConfig.java
+++ src/main/java/com/spring/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.spring.core;
 
+import com.spring.core.discount.DiscountPolicy;
 import com.spring.core.discount.FixDiscountPolicy;
+import com.spring.core.discount.RateDiscountPolicy;
 import com.spring.core.member.MemberService;
 import com.spring.core.member.MemberServiceImpl;
 import com.spring.core.member.MemoryMemberRepository;
@@ -9,14 +11,24 @@ import com.spring.core.order.OrderServiceImpl;
 
 /**
  * 나의 애플리케이션 전체를 설정하고 구성
+ * IoC 를 일으키고 있다. = IoC 컨테이너 or DI 컨테이너
  */
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
